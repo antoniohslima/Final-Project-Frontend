914a4a9ea736e65a1d18b849337ef3c074abdbc1
AntonioHSLima <anhenrique4@gmail.com>
2022-11-23 08:56:48 -0300

    deleting non logic codes

--- js/controllers/cardCreateCtrl.js
+++ js/controllers/cardCreateCtrl.js
@@ -18,8 +18,6 @@ myApp.controller("cardCreateCtrl", ['$scope', "ClientsCardService", "ClientsServ
     return 
   }
   
-  // $scope.minDate = moment().format('yyyy-MM-DD');
-  // $scope.maxDate = moment().add(10, 'years').format('yyyy-MM-DD');
   
   $scope.listNetworks = () => {
     return NetworkService.getNetworks()
@@ -29,7 +27,6 @@ myApp.controller("cardCreateCtrl", ['$scope', "ClientsCardService", "ClientsServ
   }
   
   $scope.submitCard = (card, network) => {
-    // card.expiration_date = moment(card.expiration_date).format('MM/DD/yyy');
     card.network = network.name;
     
     return ClientsCardService.createCard(clientId, card)
@@ -42,37 +39,4 @@ myApp.controller("cardCreateCtrl", ['$scope', "ClientsCardService", "ClientsServ
   $scope.listNetworks();
   $scope.setCardStyle($scope.type);
   
-  
-  // const getNetWorth = (id) => {
-  //   ClientsService.showClient(id)
-  //     .then(resp => {
-  //       const net_worth = resp.data.net_worth;
-  
-  //       if(net_worth >= 10000000) {
-  //         $scope.type = 'Full';
-  //         cardHTML.classList.add('full');
-  //         cardBackHTML.classList.add('full-back');
-  
-  //       }
-  //       else if (net_worth > 1000000){
-  //         $scope.type = 'Black';
-  //         cardHTML.classList.add('black');
-  //         cardBackHTML.classList.add('black-back');
-  //       }
-  //       else if (net_worth > 100000){
-  //         $scope.type = 'Platinum';
-  //         cardHTML.classList.add('platinum');
-  //         cardBackHTML.classList.add('platinum-back');
-  //       }
-  //       else {
-  //         $scope.type = 'Gold';
-  //         cardHTML.classList.add('gold');
-  //         cardBackHTML.classList.add('gold-back');
-  //       }
-  //     })
-    
-    
-  // }
-  
-  // getNetWorth(clientId);
 }]);
